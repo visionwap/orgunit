1e8c337764b55c6fb19058245c2f369f24511a8c
Luis David Salazar <visionwap@gmail.com>
2018-04-21 10:42:22 -0500

    Se agregan los archivos iniciales

---
+++ src/main/java/como/visionwap/orgunit/OrgunitApplication.java
@@ -0,0 +1,12 @@
+package como.visionwap.orgunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgunitApplication.class, args);
+	}
+}
---
+++ src/test/java/como/visionwap/orgunit/OrgunitApplicationTests.java
@@ -0,0 +1,16 @@
+package como.visionwap.orgunit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrgunitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
